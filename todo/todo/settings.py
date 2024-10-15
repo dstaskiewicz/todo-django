d8f7380428c6ed2bf444c04d363274a93267fa25
dstaskiewicz <dstaskiewicz5@gmail.com>
2024-10-15 21:14:03 +0200

    #12 database models changes
    - Added a GameSeries, Game, User and UserGame models

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'todogamesapp',
+    'todogamesapp.apps.TodogamesappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
