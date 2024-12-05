e58525ba5c56fdbc2946a23f7ce519cd7afa9386
dstaskiewicz <dstaskiewicz5@gmail.com>
2024-12-05 14:59:02 +0100

    #16 admin page update updated the admin page to view information more clearly and fixed usergame not being a model

--- todo/todogamesapp/admin.py
+++ todo/todogamesapp/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from todogamesapp.models import User, Game, GameSeries, UserGame
 
 # Register your models here.
+
+class GameAdmin(admin.ModelAdmin):
+    list_display = ["name", "series_id", "genre", "release_year", "image"]
+    search_fields = ["name"]
+    list_filter = ["genre"]
+
+class GameSeriesAdmin(admin.ModelAdmin):
+    list_display = ["name", "total_number_of_games", "genre", "developer"]
+    search_fields = ["name"]
+    list_filter = ["genre"]
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["name", "id", "email", "time_created"]
+    search_fields = ["name", "email"]
+    list_filter = ["time_created"]
+
+class UserGameAdmin(admin.ModelAdmin):
+    list_display = ["user_id", "game_id", "status", "started_date", "completed_date"]
+    search_fields = ["user_id__exact"]
+    list_filter = ["status"]
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Game, GameAdmin)
+admin.site.register(GameSeries, GameSeriesAdmin)
+admin.site.register(UserGame, UserGameAdmin)
--- todo/todogamesapp/models.py
+++ todo/todogamesapp/models.py
@@ -1,35 +1,54 @@
 from django.db import models
+from django.contrib import admin
+import os
 
 # Create your models here.
 # TODO: specify the image dictionary
-image_dict = ''
+game_image_dict = 'games'
+series_image_dict = 'series'
 
 class GameSeries(models.Model):
     """
     A game series model.\n
     name, first_release_date, last_release_date, total_number_of_games, genre, developer, image,
     """
+    def upload_location(instance, filename):
+        filebase, extension = os.path.splitext(filename)
+        return f'{series_image_dict}/{instance.name}{extension}'
+    
+    
     name = models.CharField(max_length=200)
     first_release_date = models.DateField()
     last_release_date = models.DateField()
     total_number_of_games = models.IntegerField()
     genre = models.CharField(max_length=200) # TODO: think about converting to choices
     developer = models.CharField(max_length=200)
-    image = models.ImageField(image_dict)
+    image = models.ImageField(upload_to=upload_location)
 
+    def __str__(self):
+        return self.name
 
 class Game(models.Model):
     """
     A game model.\n
-    name, image, howlongtobeat, release date, genre, series_id,
+    name, image, time_to_beat, release_year, genre, series_id,
     """
+    def upload_location(instance, filename):
+        filebase, extension = os.path.splitext(filename)
+        return f'{game_image_dict}/{instance.name}{extension}'
+    
+
     name = models.CharField(max_length=200)
-    image = models.ImageField(image_dict)
+    image = models.ImageField(upload_to=upload_location)
     release_year = models.IntegerField()
     time_to_beat = models.DecimalField(max_digits=5, decimal_places=2) # up to 999.99
     genre = models.CharField(max_length=200) # TODO: think about converting to choices
     series_id = models.ForeignKey(to=GameSeries, on_delete=models.CASCADE) # TODO: think about standalone games
-# IGDB API
+    
+    def __str__(self):
+        return self.name
+    
+    # IGDB API
 
 
 class User(models.Model):
@@ -42,8 +61,10 @@ class User(models.Model):
     password_hash = models.CharField(max_length=200)
     time_created = models.DateTimeField()
 
+    def __str__(self):
+        return self.name
 
-class UserGame():
+class UserGame(models.Model):
     """
     A model so user can see his games and their progress.\n
     user_id, game_id, status, started_date, completed_date,
